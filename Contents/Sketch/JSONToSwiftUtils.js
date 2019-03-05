a4ad93cf0a556dd77fb3300744e4af62cd4a3696
Paul Jones <pauljones@Pauls-MacBook-Pro.local>
2019-03-05 16:44:10 -0500

    Refactored color code gen

--- Contents/Sketch/JSONToSwiftUtils.js
+++ Contents/Sketch/JSONToSwiftUtils.js
@@ -1,15 +1,5 @@
 @import "utils.js"
 
-function MSColorAssetToSwiftColorLiteralEntry(colorAsset) {
-    var red = colorAsset["red"]
-    var green = colorAsset["green"]
-    var blue = colorAsset["blue"]
-    var alpha = colorAsset["alpha"]
-    var name = camelize(colorAsset["name"]) || `${rgbToHex(red, green, blue)}`.substr(1)
-    var entry = `public static let ${name} = #colorLiteral(red: ${red}, green: ${green}, blue: ${blue}, alpha: ${alpha}).colorName`
-    return entry
-}
-
 function MSColorAssetToSwiftColorComment(colorAsset) {
 	var red = colorAsset["red"]
     var green = colorAsset["green"]
--- Contents/Sketch/SwiftColorsFileForColorAssets.js
+++ Contents/Sketch/SwiftColorsFileForColorAssets.js
@@ -1,66 +1,36 @@
+@import "utils.js"
+
 function SwiftColorsFileForColorAssets(colorAssets) {
-    var colorAssetsCodeEntries = ""
-    for (x in colorAssets) {
-        // colorAssetsCodeEntries += `  \/\/ ${ MSColorAssetToSwiftColorComment(colorAssets[x]) } \n`
-        colorAssetsCodeEntries += `  ${ MSColorAssetToSwiftColorLiteralEntry(colorAssets[x]) } \n`
+    var colorNamesCode = ""
+    var colorCasesCode = ""
+    for (colorAssetIndex in colorAssets) {
+        var colorAsset = colorAssets[colorAssetIndex]
+        var colorAssetName = camelize(colorAsset.name)
+        colorNamesCode += `    case ${colorAssetName}\n`
+        colorCasesCode += `        case .${colorAssetName}:
+            return ${MSColorAssetToSwiftColorLiteral(colorAsset)}\n`
     }
 
-    var swiftColorsTemplateString = `
-\/\/ swiftlint:disable all
+    var swiftColorsTemplateString = `import UIKit
 
-#if os(OSX)
-  import AppKit.NSColor
-  public typealias Color = NSColor
-#elseif os(iOS) || os(tvOS) || os(watchOS)
-  import UIKit.UIColor
-  public typealias Color = UIColor
-#endif
-
-\/\/ swiftlint:disable superfluous_disable_command
-\/\/ swiftlint:disable file_length
-
-\/\/ MARK: - Colors
-
-\/\/ swiftlint:disable identifier_name line_length type_body_length
-public struct ColorName {
-  public let rgbaValue: UInt32
-  public var color: Color { return Color(named: self) }
-{{colors}}
-}
-\/\/ swiftlint:enable identifier_name line_length type_body_length
-
-\/\/ MARK: - Implementation Details
-
-\/\/ swiftlint:disable operator_usage_whitespace
-public extension Color {
-
-  var colorName: ColorName {
-    var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0, a: CGFloat = 0
-    getRed(&r, green: &g, blue: &b, alpha: &a)
-    let rgba: UInt32 = 
-        (UInt32)(max(r, 0) * 255) << 24 | 
-        (UInt32)(max(g, 0) * 255) << 16 | 
-        (UInt32)(max(b, 0) * 255) << 8 | 
-        (UInt32)(max(a, 0) * 255) << 0
-    return ColorName(rgbaValue: rgba)
-  }
-
-  convenience init(rgbaValue: UInt32) {
-    let red   = CGFloat((rgbaValue >> 24) & 0xff) / 255.0
-    let green = CGFloat((rgbaValue >> 16) & 0xff) / 255.0
-    let blue  = CGFloat((rgbaValue >>  8) & 0xff) / 255.0
-    let alpha = CGFloat((rgbaValue      ) & 0xff) / 255.0
-
-    self.init(red: red, green: green, blue: blue, alpha: alpha)
-  }
-}
-\/\/ swiftlint:enable operator_usage_whitespace
-
-public extension Color {
-  convenience init(named color: ColorName) {
-    self.init(rgbaValue: color.rgbaValue)
-  }
+public enum ColorName: String, CaseIterable {
+{{colorNames}}
+    var color: UIColor {
+        switch self {
+{{colorCases}}
+        }
+    }
 }
 `
-    return swiftColorsTemplateString.replace(/{{colors}}/g, colorAssetsCodeEntries)
+    return swiftColorsTemplateString.replace(/{{colorNames}}/g, colorNamesCode).replace(/{{colorCases}}/g, colorCasesCode)
 }
+
+function MSColorAssetToSwiftColorLiteral(colorAsset) {
+    var red = colorAsset["red"]
+    var green = colorAsset["green"]
+    var blue = colorAsset["blue"]
+    var alpha = colorAsset["alpha"]
+    var entry = `#colorLiteral(red: ${red}, green: ${green}, blue: ${blue}, alpha: ${alpha})`
+    return entry
+}
+
